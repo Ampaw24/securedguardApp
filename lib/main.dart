73e4a640420957c350b03860aac558241ea05dc9
Ampaw24 <ampawsafo22@gmail.com>
2023-09-08 07:55:43 +0000

    firebase added sucess for app

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,15 @@
 
 import 'package:flutter/material.dart';
 import 'screens/splashscreen/splashscreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
